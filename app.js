98af6d697952a36f63a3ef3999f7cd5a6d2d898e
6kotov <66kotov@gmail.com>
2020-01-03 17:17:52 +0200

    Test authentication\authorisation system created.

--- app.js
+++ app.js
@@ -1,80 +1,116 @@
+if (process.env.NODE_ENV !== 'production') {
+ require('dotenv').config()
+ require('dotenv').parse('/.env')
+}
+
+
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json()
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
+const bcrypt = require('bcrypt')
+const users = []
+const flash = require('express-flash')
+const session = require('express-session')
+const passport = require('passport')
+const methodOverride = require('method-override')
+const initializePassport = require('./passport-config')
+const mongoose = require('mongoose')
 
-app.use((req, res, next) => {
- res.header('Access-Control-Allow-Origin', '*');
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true,  useUnifiedTopology: true  })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
 
- // authorized headers for preflight requests
- // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
- res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
- next();
-
- app.options('*', (req, res) => {
-  // allowed XHR methods
-  res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
-  res.send();
- });
-});
-
+initializePassport(
+              passport,
+email => users.find(user => user.email === email),
+id => users.find(user => user.id === id)
+)
 
 app.set('view engine', 'ejs');
+app.set('views',__dirname+'/views')
+app.use(express.urlencoded({ extended: false }))
 app.use('/public', express.static('public'));
+app.use(flash())
+app.use(session({
+ secret: process.env.SESSION_SECRET,
+ resave: false,
+ saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(express.json())
+app.use(methodOverride('_method'))
 
 
-app.get('/',function (req,res) {
- res.render('index')
-})
-
-app.post('/', jsonParser ,function (req,res) {
- res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
- if(!req.body) { res.sendStatus(400)}
- console.log(req.body)
- res.render('index')
-})
-
-app.post('/about',function (req,res) {
- if(!req.body) { res.sendStatus(400)}
- console.log(req)
- res.render('index')
-})
-
-app.get('/news/:id',function (req,res) {
-
- let obj = {title:"News",
-            id: 46,
-             pharagrafs:["Первый","Второй","Третий","Четверый"]}
- res.render('news', {newsId: req.params.id, data:obj})
-})
-
-app.get('/about',function (req,res) {
- res.render('about')
-})
-
-app.listen(3000)
-
-
-
-
-
-
-// let server = http.createServer((req, res) => {
+// app.use((req, res, next) => {
+//  res.header('Access-Control-Allow-Origin', '*');
 //
-//  if(req.url === '/home' || req.url === '/'){
-//   res.writeHead(200, {'Content-type':'text/html; charset=utf-8'})
-//   let read_part = fs.createReadStream(__dirname+'/index.ejs')
-//   read_part.pipe(res)
-//  } else if (req.url === '/about'){
-//   res.writeHead(200, {'Content-type':'text/html; charset=utf-8'})
-//   let read_part = fs.createReadStream(__dirname+'/about.ejs')
-//   read_part.pipe(res)
-//  } else {
-//   res.writeHead(404, {'Content-type':'text/html; charset=utf-8'})
-//   let read_part = fs.createReadStream(__dirname+'/404.html')
-//   read_part.pipe(res)
-//  }
+//  // authorized headers for preflight requests
+//  // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+//  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//  next();
 //
-// })
-// server.listen(3000, '127.0.0.1')
+//  app.options('*', (req, res) => {
+//   // allowed XHR methods
+//   res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+//   res.send();
+//  });
+// });
+
+
+
+
+app.get('/', checkAuthenticated, function (req,res) {
+ res.render('index',{name:req.user.name})
+})
+app.get('/login', checkNotAuthenticated, function (req,res) {
+ res.render('login')
+})
+app.get('/register', checkNotAuthenticated, function (req,res) {
+ res.render('register')
+})
+
+app.post('/register', checkNotAuthenticated, async function(req,res){
+ try {
+const hashedPassword = await bcrypt.hash(req.body.password, 10)
+  users.push({
+   id:Date.now().toString(),
+   name: req.body.name,
+   email: req.body.email,
+   password: hashedPassword
+  })
+  res.redirect('/login')
+ } catch {
+  console.log('Error!')
+res.redirect('/register')
+ }
+ console.log(users)
+})
+
+app.post('/login', checkNotAuthenticated,  passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
+
+app.delete('/logout', (req, res)=>{
+  req.logOut()
+  res.redirect('/login')
+})
+
+ function checkAuthenticated(req, res, next) {
+   if (req.isAuthenticated()) {
+     return next()
+   }
+   res.redirect('/login')
+ }
+
+ function checkNotAuthenticated(req, res, next){
+   if (req.isAuthenticated()) {
+    return res.redirect('/')
+   }
+  next()
+ }
+
+app.listen(process.env.PORT ||3000)
+
